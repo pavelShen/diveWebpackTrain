ef63f5aebc57563414481e809a67a263e6e48f2a
shenyuchen <shenyuchen@f-road.com.cn>
2018-02-09 14:10:57 +0800

    finish 3-3

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 ## 笔记
 
+电子书地址：http://webpack.wuhaolin.cn/
+
 ### 3-1 babel
 
 还需要安装
 npm install --save-dev babel-plugin-transform-runtime
 npm install --save babel-runtime
+
+### 3-3flow
+flow需要执行flow init初始化配置
+安装vscode插件 flow language support
+关闭编辑器自带的检查 
+
+```json
+{
+  "javascript.validate.enable": false,
+}
+```
--- show.js
+++ show.js
@@ -1,7 +1,15 @@
+// @flow
+
 // 操作 DOM 元素，把 content 显示到网页上
-export function show(content) {
+export function show(content: string) {
   var f = [1,2,3].map((item)=>{
     return item * item
   })
+
+  function square1(n: number): number {
+    return n * n;
+  }
+  square1('2');
+
   window.document.getElementById('app').innerText = `Hello,${content}`;
 }
