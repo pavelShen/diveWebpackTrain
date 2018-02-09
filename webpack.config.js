c05f7cf522504af735016d9fa9787c29d3a980bb
shenyuchen <shenyuchen@f-road.com.cn>
2018-02-09 11:54:29 +0800

    finish 3-2

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,21 @@ const path = require('path');
 
 module.exports = {
   // JS 执行入口文件
-  entry: './main.js',
+  entry: './main.ts',
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
     filename: 'bundle.js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
   },
+  resolve: {
+    extensions: ['.ts','.js']
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: ['babel-loader'],
+        test: /\.ts$/,
+        use: ['awesome-typescript-loader'],
       },
     ]
   },
