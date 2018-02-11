2e67a6a1f287f07b48bdb4c459b83fa1d8799eee
shenyuchen <shenyuchen@f-road.com.cn>
2018-02-11 11:07:14 +0800

    finish 3-9

--- main.tsx
+++ main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
 
+require('./src/css/index.css')
 class Button extends Component {
   render() {
     return <h1>Hello,Webpack</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // JS 执行入口文件
@@ -18,7 +22,36 @@ module.exports = {
         test: /\.tsx?$/,
         use: ['awesome-typescript-loader'],
       },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader?minimize']
+        })
+      }
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        output: {
+          comments: false,
+          beautify: false
+        },
+      }
+    }),
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template:'src/index.html',
+      title:'testTemplate'
+    }),
+    new ExtractTextPlugin({
+      filename: '[name]_[contenthash:8].css'
+    })
+  ],
   devtool: 'source-map' // 输出 source-map 方便直接调试 ES6 源码
 };
